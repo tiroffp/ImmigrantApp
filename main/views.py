bdc0c8378e122fb5d494ef902c092ee87685463f
Peyton Tiroff <tiroffp@gmail.com>
2016-11-28 16:03:28 -0500

    made dictionary of filter form get request

--- main/views.py
+++ main/views.py
@@ -5,6 +5,16 @@ from main.forms import ImmFilterForm
 
 # Create your views here.
 def home_page(request):
+    data = {
+        'firstname':request.GET.get('firstname',''),
+        'lastname':request.GET.get('lastname',''),
+        'gender':request.GET.get('gender',''),
+        'country':request.GET.get('country',''),
+        'ethnicity':request.GET.get('ethnicity',''),
+        'spokenlang':request.GET.get('spokenlang',''),
+        'processLocation':request.GET.get('processLocation',''),
+    }
+
+    form = ImmFilterForm(data)
     immigrants = getAllImmigrants()
-    form = ImmFilterForm()
     return render(request, 'index.html', {'immigrants': immigrants, 'form' : form})
