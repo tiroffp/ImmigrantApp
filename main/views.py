e288ada1051536618d35a6e50993d8a53ac0e580
cmullaney12 <cmullaney12@cox.net>
2016-11-28 15:35:00 -0500

    Updated sql functions to return objects, created new filter form, added for to index file

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from immigrantApp.sqlFunctions import getAllImmigrants
+from main.forms import ImmFilterForm
 
 
 # Create your views here.
 def home_page(request):
     immigrants = getAllImmigrants()
-    return render(request, 'index.html', {'immigrants': immigrants})
+    form = ImmFilterForm()
+    return render(request, 'index.html', {'immigrants': immigrants, 'form' : form})
